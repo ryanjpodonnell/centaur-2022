6f3e0ba4d01f937cfa732d424f72ad413f681365
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2022-01-24 22:24:37 -0800

    move functions to GameMode class

--- GameMode.cpp
+++ GameMode.cpp
@@ -1,11 +1,94 @@
 #include <Arduino.h>
 #include "BSOS_Config.h"
 #include "BallySternOS.h"
+#include "CountdownBonusState.h"
 #include "Display.h"
 #include "GameMode.h"
+#include "Lamps.h"
 #include "MachineState.h"
 #include "PinballMachineBase.h"
+#include "SelfTestAndAudit.h"
 #include "SharedVariables.h"
+
+int InitGamePlay() {
+  if (DEBUG_MESSAGES) {
+    Serial.write("Starting game\n\r");
+  }
+
+  BSOS_SetDisableFlippers(false);
+  BSOS_EnableSolenoidStack();
+  BSOS_TurnOffAllLamps();
+
+  BSOS_PushToTimedSolenoidStack(SOL_ORBS_TARGET_RESET, 10, CurrentTime + 500);
+  BSOS_PushToTimedSolenoidStack(SOL_INLINE_DROP_TARGET_RESET, 10, CurrentTime + 500);
+  BSOS_PushToTimedSolenoidStack(SOL_4_RIGHT_DROP_TARGET_RESET, 10, CurrentTime + 500);
+
+  for (int count = 0; count < 4; count++) {
+    BonusX[count] = 1;
+    Bonus[count] = 0;
+  }
+  memset(CurrentScores, 0, 4 * sizeof(unsigned long));
+
+  SamePlayerShootsAgain = false;
+  CurrentBallInPlay = 1;
+  CurrentNumPlayers = 1;
+  CurrentPlayer = 0;
+  ShowPlayerScores(0xFF, false, false);
+
+  return MACHINE_STATE_INIT_NEW_BALL;
+}
+
+int InitNewBall(bool curStateChanged, byte playerNum, int ballNum) {
+  if (curStateChanged) {
+    ShowLamps(LAMP_COLLECTION_BONUS_ALL, true);
+
+    BallFirstSwitchHitTime = 0;
+
+    BSOS_SetDisplayCredits(Credits, true);
+    SamePlayerShootsAgain = false;
+
+    BSOS_SetDisplayBallInPlay(ballNum);
+    BSOS_SetLampState(LAMP_TILT, 0);
+
+    if (BallSaveNumSeconds > 0) {
+      BSOS_SetLampState(LAMP_SHOOT_AGAIN, 1, 0, 500);
+    }
+
+    BallSaveUsed = false;
+    BallTimeInTrough = 0;
+    NumTiltWarnings = 0;
+    LastTiltWarningTime = 0;
+
+    // Initialize game-specific start-of-ball lights & variables
+    GameModeStartTime = 0;
+    GameModeEndTime = 0;
+    GameMode = GAME_MODE_SKILL_SHOT;
+
+    ExtraBallCollected = false;
+
+    if (BSOS_ReadSingleSwitchState(SW_OUTHOLE)) {
+      BSOS_PushToTimedSolenoidStack(SOL_OUTHOLE_KICKER, 4, CurrentTime + 600);
+    }
+
+    // Reset progress unless holdover awards
+    Bonus[CurrentPlayer] = 0;
+    BonusX[CurrentPlayer] = 1;
+
+    ScoreMultiplier = 1;
+    CurrentBonus = Bonus[CurrentPlayer];
+    ScoreAdditionAnimation = 0;
+    ScoreAdditionAnimationStartTime = 0;
+    BonusXAnimationStart = 0;
+  }
+
+  // We should only consider the ball initialized when
+  // the ball is no longer triggering the SW_OUTHOLE
+  if (BSOS_ReadSingleSwitchState(SW_OUTHOLE)) {
+    return MACHINE_STATE_INIT_NEW_BALL;
+  } else {
+    return MACHINE_STATE_NORMAL_GAMEPLAY;
+  }
+}
 
 int ManageGameMode() {
   int returnState = MACHINE_STATE_NORMAL_GAMEPLAY;
@@ -61,6 +144,128 @@ int ManageGameMode() {
     }
   } else {
     BallTimeInTrough = 0;
+  }
+
+  return returnState;
+}
+
+int RunGamePlayMode(int curState, boolean curStateChanged) {
+  int returnState = curState;
+
+  // Very first time into gameplay loop
+  if (curState == MACHINE_STATE_INIT_GAMEPLAY) {
+    returnState = InitGamePlay();
+  } else if (curState == MACHINE_STATE_INIT_NEW_BALL) {
+    returnState = InitNewBall(curStateChanged, CurrentPlayer, CurrentBallInPlay);
+  } else if (curState == MACHINE_STATE_NORMAL_GAMEPLAY) {
+    returnState = ManageGameMode();
+  } else if (curState == MACHINE_STATE_COUNTDOWN_BONUS) {
+    returnState = CountdownBonus(curStateChanged);
+    ShowPlayerScores(0xFF, false, false);
+  } else if (curState == MACHINE_STATE_BALL_OVER) {
+    BSOS_SetDisplayCredits(Credits);
+
+    if (SamePlayerShootsAgain) {
+      returnState = MACHINE_STATE_INIT_NEW_BALL;
+    } else {
+      CurrentPlayer += 1;
+      if (CurrentPlayer >= CurrentNumPlayers) {
+        CurrentPlayer = 0;
+        CurrentBallInPlay += 1;
+      }
+
+      if (CurrentBallInPlay > BallsPerGame) {
+        for (int count = 0; count < CurrentNumPlayers; count++) {
+          BSOS_SetDisplay(count, CurrentScores[count], true, 2);
+        }
+
+        returnState = MACHINE_STATE_INIT_GAMEPLAY;
+      }
+      else returnState = MACHINE_STATE_INIT_NEW_BALL;
+    }
+  } else if (curState == MACHINE_STATE_MATCH_MODE) {
+  }
+
+  byte switchHit;
+  if (NumTiltWarnings <= MaxTiltWarnings) {
+    while ( (switchHit = BSOS_PullFirstFromSwitchStack()) != SWITCH_STACK_EMPTY ) {
+
+      if (DEBUG_MESSAGES) {
+        char buf[128];
+        sprintf(buf, "Switch Hit = %d\n", switchHit);
+        Serial.write(buf);
+      }
+
+      switch (switchHit) {
+        case SW_TILT:
+          // This should be debounced
+          if ((CurrentTime - LastTiltWarningTime) > TILT_WARNING_DEBOUNCE_TIME) {
+            LastTiltWarningTime = CurrentTime;
+            NumTiltWarnings += 1;
+            if (NumTiltWarnings > MaxTiltWarnings) {
+              BSOS_DisableSolenoidStack();
+              BSOS_SetDisableFlippers(true);
+              BSOS_TurnOffAllLamps();
+              BSOS_SetLampState(LAMP_TILT, 1);
+            }
+          }
+          break;
+        case SW_SELF_TEST_SWITCH:
+          returnState = MACHINE_STATE_TEST_LIGHTS;
+          SetLastSelfTestChangedTime(CurrentTime);
+          break;
+        case SW_LEFT_SLINGSHOT:
+        case SW_RIGHT_SLINGSHOT:
+          CurrentScores[CurrentPlayer] += 10;
+          if (BallFirstSwitchHitTime == 0) BallFirstSwitchHitTime = CurrentTime;
+          break;
+        case SW_COIN_1:
+        case SW_COIN_2:
+        case SW_COIN_3:
+          AddCoinToAudit(switchHit);
+          AddCredit(true, 1);
+          break;
+        case SW_CREDIT_BUTTON:
+          if (CurrentBallInPlay < 2) {
+            // If we haven't finished the first ball, we can add players
+            AddPlayer();
+          } else {
+            // If the first ball is over, pressing start again resets the game
+            if (Credits >= 1 || FreePlayMode) {
+              if (!FreePlayMode) {
+                Credits -= 1;
+                BSOS_WriteByteToEEProm(BSOS_CREDITS_EEPROM_BYTE, Credits);
+                BSOS_SetDisplayCredits(Credits);
+              }
+              returnState = MACHINE_STATE_INIT_GAMEPLAY;
+            }
+          }
+          if (DEBUG_MESSAGES) {
+            Serial.write("Start game button pressed\n\r");
+          }
+          break;
+      }
+    }
+  } else {
+    // We're tilted, so just wait for outhole
+    switchHit = BSOS_PullFirstFromSwitchStack();
+
+    while (switchHit != SWITCH_STACK_EMPTY) {
+      switch (switchHit) {
+        case SW_SELF_TEST_SWITCH:
+          returnState = MACHINE_STATE_TEST_LIGHTS;
+          SetLastSelfTestChangedTime(CurrentTime);
+          break;
+        case SW_COIN_1:
+        case SW_COIN_2:
+        case SW_COIN_3:
+          AddCoinToAudit(switchHit);
+          AddCredit(true, 1);
+          break;
+      }
+
+      switchHit = BSOS_PullFirstFromSwitchStack();
+    }
   }
 
   return returnState;
--- GameMode.h
+++ GameMode.h
@@ -2,8 +2,12 @@
 
 #define GAME_MODE_SKILL_SHOT        0
 #define GAME_MODE_UNSTRUCTURED_PLAY 1
+#define TILT_WARNING_DEBOUNCE_TIME 1000
 
+int InitGamePlay();
+int InitNewBall(bool curStateChanged, byte playerNum, int ballNum);
 int ManageGameMode();
+int RunGamePlayMode(int curState, boolean curStateChanged);
 void SetGameMode(byte newGameMode);
 
 #define GAME_MODE_H
