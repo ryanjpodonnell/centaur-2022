83db5d40e4111cee6d02e1a29a91e068a0b67c08
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2022-07-08 17:14:48 -0700

    Show knocked down right arrows

--- PlayerState.cpp
+++ PlayerState.cpp
@@ -654,36 +654,15 @@ void PlayerState::updateQueensChamberLamps() {
 }
 
 void PlayerState::updateRightDropTargetsLamps() {
-  if (activeRightDropTarget_ == 0xFF) {
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_10_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_20_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_40_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_80_ARROW);
-  }
-  if (activeRightDropTarget_ == SW_RIGHT_4_DROP_TARGET_1) {
-    g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_10_ARROW, true);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_20_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_40_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_80_ARROW);
-  }
-  if (activeRightDropTarget_ == SW_RIGHT_4_DROP_TARGET_2) {
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_10_ARROW);
-    g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_20_ARROW, true);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_40_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_80_ARROW);
-  }
-  if (activeRightDropTarget_ == SW_RIGHT_4_DROP_TARGET_3) {
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_10_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_20_ARROW);
-    g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_40_ARROW, true);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_80_ARROW);
-  }
-  if (activeRightDropTarget_ == SW_RIGHT_4_DROP_TARGET_4) {
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_10_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_20_ARROW);
-    g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_40_ARROW);
-    g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_80_ARROW, true);
-  }
+  if (activeRightDropTarget_ == SW_RIGHT_4_DROP_TARGET_1) g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_10_ARROW, true);
+  if (activeRightDropTarget_ == SW_RIGHT_4_DROP_TARGET_2) g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_20_ARROW, true);
+  if (activeRightDropTarget_ == SW_RIGHT_4_DROP_TARGET_3) g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_40_ARROW, true);
+  if (activeRightDropTarget_ == SW_RIGHT_4_DROP_TARGET_4) g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_80_ARROW, true);
+
+  rightDropTargets_[0] ? g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_10_ARROW) : g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_10_ARROW);
+  rightDropTargets_[1] ? g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_20_ARROW) : g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_20_ARROW);
+  rightDropTargets_[2] ? g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_40_ARROW) : g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_40_ARROW);
+  rightDropTargets_[3] ? g_lampsHelper.showLamp(LAMP_RIGHT_DROP_TARGET_80_ARROW) : g_lampsHelper.hideLamp(LAMP_RIGHT_DROP_TARGET_80_ARROW);
 }
 
 void PlayerState::updateRightDropTargetsResetLamp() {
